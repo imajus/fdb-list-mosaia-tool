4ccea11f988f217d980b8add37d282c6c2d2d7f8
Denis Perov <denis.perov@gmail.com>
2025-07-01 11:06:31 +0300

    Implement tool

--- bin/dev.js
+++ bin/dev.js
@@ -6,34 +6,14 @@ dotenv.config();
 
 const app = express();
 
-const { ENV_VAR_ONE, PORT } = process.env;
-
-if(ENV_VAR_ONE === undefined) {
-    console.log('`ENV_VAR_ONE` not set. Copy .env.example to .env first.');
-    process.exit(1);
-}
+const { PORT } = process.env;
 
 app.get('/', async (req, res) => {
-    const { EXAMPLE_PARAM_ONE, EXAMPLE_PARAM_TWO } = req.query;
-
-    const event = {
-        body: JSON.stringify({
-            args: {
-                EXAMPLE_PARAM_ONE,
-                EXAMPLE_PARAM_TWO
-            },
-            secrets: {
-                ENV_VAR_ONE
-            }
-        })
-    }
-
-    const result = await handler(event)
-
-    res.status(result.statusCode).send(result.body);
+  const result = await handler();
+  res.status(result.statusCode).send(result.body);
 });
 
 const port = PORT || 3000;
 app.listen(port, () => {
-    console.log(`Local development server running on port ${port}`);
+  console.log(`Local development server running on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,22 @@
-import toolCall from "./tool-call";
-
-type RawEvent = {
-    body: string;
-}
-
-type ParsedEvent = {
-    args: Record<string, string>;
-    secrets: Record<string, string>;
-}
+import toolCall from './tool-call';
 
 export async function handler() {
-    try {
-        const result = await toolCall();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(result),
-        };
-    } catch (error: unknown) {
-        let message = '';
-        if (error instanceof Error) {
-            message = error.message;
-        } else {
-            message = 'Unknown error';
-        }
-        return {
-            statusCode: 500,
-            body: JSON.stringify(message),
-        };
+  try {
+    const result = await toolCall();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error: unknown) {
+    let message = '';
+    if (error instanceof Error) {
+      message = error.message;
+    } else {
+      message = 'Unknown error';
     }
+    return {
+      statusCode: 500,
+      body: JSON.stringify(message),
+    };
+  }
 }
